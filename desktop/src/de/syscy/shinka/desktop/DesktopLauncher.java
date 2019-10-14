d4cb16301fe8ecc98e7ccc126dc89296384fd616
Kage0x3B <kage0x3b@outlook.com>
2019-10-14 09:49:41 +0200

    Initial libGDX project

---
+++ desktop/src/de/syscy/shinka/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.syscy.shinka.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.syscy.shinka.ShinkaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShinkaGame(), config);
+	}
+}
